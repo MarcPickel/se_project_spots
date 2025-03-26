27fe653a452bbcf8e5eacb95fae5a66d03e9f218
Marc Pickelman <marcpc@att.net>
2025-03-26 12:45:03 -0500

    Add index.js directory and <script> above </body>; add code to index.js

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+const initialCards = ["val", "terrace", "cafe", "bridge", "tunnel", "house"];
+
+let val = {
+  name: "Val Thorens",
+  url: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+};
+
+let;
+terrace = {
+  name: "Restaurant terrace",
+  url: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+};
+
+let cafe = {
+  name: "An outdoor cafe",
+  url: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+};
+
+let bridge = {
+  name: "A very long bridge, over the forest and through the trees",
+  url: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+};
+
+let tunnel = {
+  name: "Tunnel with morning light",
+  url: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+};
+
+let house = {
+  name: "Mountain house",
+  url: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+};
